3e411174563b908ad740581140f9511b5e08c572
mturilin <mturilin@gmail.com>
2014-08-20 18:38:56 -0700

    0.0.2 - code cleanup

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+# 0.0.2
+Date: 08/20/2014
+Changes:
+
+- Code cleanup
+
 # 0.0.1
 Date: 08/20/2014
 Changes:
--- lib/transformer.dart
+++ lib/transformer.dart
@@ -1,10 +1,6 @@
 /*
- * Copyright (c) 2014, adalberto.lacruz@gmail.com
- * Thanks to juha.komulainen@evident.fi for inspiration and some code
- * (Copyright (c) 2013 Evident Solutions Oy) from package http://pub.dartlang.org/packages/sass
- *
- * v 0.1.1  20140521 compatibility with barback (0.13.0) and lessc (1.7.0)
- * v 0.1.0  20140218
+ * Copyright (c) 2014, mikhail@turilin.com
+ * (based on https://github.com/AdalbertoLacruz/less_node - thanks a lot)
  */
 library lesscompile;
 
@@ -14,36 +10,20 @@ import 'dart:isolate';
 import 'dart:convert';
 import 'package:barback/barback.dart';
 
-/*
- * Transformer used by 'pub build' to convert .less files to .css
- * Based on lessc over nodejs executing a process like
- * CMD> lessc --flags input.less > output.css
- * Uses only one file as entry point and produces only one css file
- * To mix several .less files, the input contents could have "@import 'filexx.less'; ..." directives
- * See http://lesscss.org/ for more information
- */
+
 class LessTransformer extends Transformer {
     final BarbackSettings settings;
     bool cleancss = false;
 
-    // cleancss: true - compress output by using clean-css
     bool compress = false;
 
-    // compress: true - compress output by removing some whitespaces
     String start_dir = '';
 
-    // start_dir: web/ - main folder to start
     String executable = 'lessc';
 
-    //executable: lessc - command to execute lessc
     String include_path = '';
 
-
     List<String> files;
-
-    //to build process arguments
-
-//    String get allowedExtensions => ".less";
 
     LessTransformer.asPlugin(this.settings) {
         var args = settings.configuration;
@@ -95,10 +75,8 @@ class LessTransformer extends Transformer {
 
 
 }
-/* ************************************** */
-/*
- * process error management
- */
+
+
 class LessException implements Exception {
     final String message;
 
